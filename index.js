f9f38112b065e85f9ce79023935ad4950ddd169e
Andrew McRitchie <andrewgmcritchie@gmail.com>
2023-01-24 14:06:11 -0800

    hardcoded account lookup apis

--- index.js
+++ index.js
@@ -6,12 +6,21 @@ app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/api/users', (req, res) => {
+app.get('/api/users', (req, res) => {
     console.log(req.body);
-    if (req.body.name === "John") {
-        res.json([{ name: 'John Doe' }])
+    if (req.body.account === "1111") {
+        res.json({message: '$12,000', found: 'yes'})
     }
-    res.json([{ name: 'Jane Doe' }])
+    if (req.body.account === "2222") {
+      res.json({message: '$834.65', found: 'yes'})
+    }
+    if (req.body.account === "3333") {
+      res.json({message: '$9,329', found: 'yes'})
+    }
+    if (req.body.acount === "4444") {
+      res.json({message: '$208.45', found: 'yes'})
+    }
+    res.json({ message: `I'm sorry your account number does not match any of our records`, found: 'no'})
 })
 
 app.listen(3000, () => {
