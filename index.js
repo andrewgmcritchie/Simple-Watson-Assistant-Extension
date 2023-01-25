94f7df7b340ab1bcd5901f0861cc49b8b87c0875
Andrew McRitchie <andrewgmcritchie@gmail.com>
2023-01-25 11:20:26 -0800

    Changed to req query

--- index.js
+++ index.js
@@ -6,18 +6,18 @@ app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/api/users', (req, res) => {
-    console.log(req.body.account);
-    if (req.body.account === '1111') {
+app.get('/api/users', (req, res) => {
+    console.log(req.query.account);
+    if (req.query.account === '1111') {
         res.json({message: '$12,000', found: 'yes'})
     }
-    if (req.body.account === '2222') {
+    if (req.query.account === '2222') {
       res.json({message: '$834.65', found: 'yes'})
     }
-    if (req.body.account === '3333') {
+    if (req.query.account === '3333') {
       res.json({message: '$9,329', found: 'yes'})
     }
-    if (req.body.acount === '4444') {
+    if (req.query.acount === '4444') {
       res.json({message: '$208.45', found: 'yes'})
     }
     res.json({ message: `I'm sorry your account number does not match any of our records`, found: 'no'})
