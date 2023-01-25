ffe99c5cf9dca2eaf647c1612f424f06b1bcc95e
Andrew McRitchie <andrewgmcritchie@gmail.com>
2023-01-25 11:18:40 -0800

    updated api

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ app.post('/api/users', (req, res) => {
     if (req.body.account === '1111') {
         res.json({message: '$12,000', found: 'yes'})
     }
-    if (req.body.account === 2222) {
+    if (req.body.account === '2222') {
       res.json({message: '$834.65', found: 'yes'})
     }
-    if (req.body.account === 3333) {
+    if (req.body.account === '3333') {
       res.json({message: '$9,329', found: 'yes'})
     }
-    if (req.body.acount === 4444) {
+    if (req.body.acount === '4444') {
       res.json({message: '$208.45', found: 'yes'})
     }
     res.json({ message: `I'm sorry your account number does not match any of our records`, found: 'no'})
